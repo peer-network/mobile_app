5ddac86ea7d6f05cb5eca38aba0d503de3fb4d95
Endata <ender@peerapp.de>
2024-10-22 12:17:13 +0200

    landing page ist entry app

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,12 @@ import 'package:peer_app/landing_page/peer_principle/principle.dart';
 import 'package:peer_app/landing_page/register_screen/register_screen.dart';
 import 'package:peer_app/newsfeed/comments/comments_screen.dart';
 import 'package:peer_app/newsfeed/newsfeed.dart';
-import 'package:peer_app/screens/chats.dart'; 
+import 'package:peer_app/screens/chats.dart';
 import 'package:peer_app/screens/create_post.dart';
 import 'package:peer_app/screens/profile.dart';
 import 'package:peer_app/screens/wallet.dart';
 import 'package:peer_app/landing_page/verify/verify.dart';
 import 'landing_page/landing page/landing_page.dart'; // Import the landing page
-
 
 void main() {
   runApp(const MyApp());
@@ -23,24 +22,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const NewsFeed(), // Set the LandingPage as the home screen
-    routes: {
-        '/register': (context) => const RegisterScreen(),
-        '/landing': (context) => const LandingPage(),
-        '/forgot': (context) => const ForgotPasswordErrorScreen(),
-        '/principle': (context) => PeerPrinciplePage(),
-        '/login': (context) => LoginPage(),
-        '/newsfeed': (context) => NewsFeed(),
-        '/wallet': (context) => WalletScreen(),
-        '/posting': (context) => PostingScreen(),
-        '/chats': (context) => ChatsScreen(),
-        '/profile': (context) => ProfileScreen(),
-        '/verify': (context) => VerifyScreen(),
-        '/comments': (context) => CommentScreen(),
-
-  }
-    );
-    
+        debugShowCheckedModeBanner: false,
+        home: LandingPage(), // Set the LandingPage as the home screen
+        routes: {
+          '/register': (context) => const RegisterScreen(),
+          '/landing': (context) => const LandingPage(),
+          '/forgot': (context) => const ForgotPasswordErrorScreen(),
+          '/principle': (context) => PeerPrinciplePage(),
+          '/login': (context) => LoginPage(),
+          '/newsfeed': (context) => NewsFeed(),
+          '/wallet': (context) => WalletScreen(),
+          '/posting': (context) => PostingScreen(),
+          '/chats': (context) => ChatsScreen(),
+          '/profile': (context) => ProfileScreen(),
+          '/verify': (context) => VerifyScreen(),
+          '/comments': (context) => CommentScreen(),
+        });
   }
 }
